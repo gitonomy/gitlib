a3f390e7d86717cdb40f03206c158005a473f292
Grégoire Pineau <lyrixx@lyrixx.info>
2013-01-17 19:02:23 +0100

    Unset the HOME env before calling git binary
    If we do no unset the env, git will use a potential
    .gitconfig which can alter results

--- src/Gitonomy/Git/Repository.php
+++ src/Gitonomy/Git/Repository.php
@@ -439,6 +439,8 @@ class Repository
 
         $builder = new ProcessBuilder(array_merge($base, $args));
 
+        $builder->inheritEnvironmentVariables(false);
+
         return $builder->getProcess();
     }
 }
