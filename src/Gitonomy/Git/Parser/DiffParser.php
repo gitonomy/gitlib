793ffe5826c30e64ea499ea9e4bb353dd0892bef
Bruce Wells <brucekwells@gmail.com>
2022-03-01 05:56:00 -0500

    Assign to variable to prevent null value (#192)

--- src/Gitonomy/Git/Parser/DiffParser.php
+++ src/Gitonomy/Git/Parser/DiffParser.php
@@ -90,8 +90,8 @@ class DiffParser extends ParserBase
 
             $oldName = $oldName === '/dev/null' ? null : substr($oldName, 2);
             $newName = $newName === '/dev/null' ? null : substr($newName, 2);
-            $oldIndex !== null ?: '';
-            $newIndex !== null ?: '';
+            $oldIndex = $oldIndex !== null ?: '';
+            $newIndex = $newIndex !== null ?: '';
             $oldIndex = preg_match('/^0+$/', $oldIndex) ? null : $oldIndex;
             $newIndex = preg_match('/^0+$/', $newIndex) ? null : $newIndex;
             $file = new File($oldName, $newName, $oldMode, $newMode, $oldIndex, $newIndex, $isBinary);
